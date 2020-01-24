6aa8846b8c90e7272a6c5fe8b17943e310437c82
Mateo Gonzalez <matteu20@hotmail.com>
2020-01-24 12:02:01 -0500

    procfile y dependencies

--- src/main/java/edu/escuelaing/arep/SparkWeb.java
+++ src/main/java/edu/escuelaing/arep/SparkWeb.java
@@ -13,5 +13,11 @@ package edu.escuelaing.arep;
 import static spark.Spark.*;
 public class SparkWeb {
     public static void main(String[] args) {
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");    }
+    
+    static int getPort() {if (System.getenv("PORT") != null){ 
+        return Integer.parseInt(System.getenv("PORT"));        }  
+    return 4567;
+    } //returns default port if heroku-port isn't set(i.e. on localhost)    }}
 }
