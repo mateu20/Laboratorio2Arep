cd136091b88a4017c895d9962e6e0e2a0553b159
Mateo Gonzalez <matteu20@hotmail.com>
2020-01-29 16:09:31 -0500

    Formularios

--- src/main/java/edu/escuelaing/arep/SparkWeb.java
+++ src/main/java/edu/escuelaing/arep/SparkWeb.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package edu.escuelaing.arep;
-
+import spark.Request;
+import spark.Response;
+import static spark.Spark.*;
 /**
  *
  * @author david.gonzalez-g
@@ -22,7 +24,31 @@ public class SparkWeb {
     
     
     }
-    
+     private static String inputDataPage(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h2>HTML Forms</h2>"
+                + "<form action=\"/results\">"
+                + "  First name:<br>"
+                + "  <input type=\"text\" name=\"firstname\" value=\"Mickey\">"
+                + "  <br>"
+                + "  Last name:<br>"
+                + "  <input type=\"text\" name=\"lastname\" value=\"Mouse\">"
+                + "  <br><br>"
+                + "  <input type=\"submit\" value=\"Submit\">"
+                + "</form>"
+                + "<p>If you click the \"Submit\" button, the form-data will be sent to a page called \"/results\".</p>"
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+
+    private static String resultsPage(Request req, Response res) {
+        return req.queryParams("firstname") + " " +
+                req.queryParams("lastname");
+    }
     
     
     static int getPort() {if (System.getenv("PORT") != null){ 
